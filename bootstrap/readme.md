91b610eaf762a4fbf5e744361eb19e84d114e35f
Anthonyrules144 <epicman56@yahoo.com>
2016-06-11 07:50:29 -0500

    Create new project

---
+++ bootstrap/readme.md
@@ -0,0 +1,4 @@
+<h1>Bootstrap</h1> [Click here to get boostrap](http://getbootstrap.com/)
+<hr>
+
+<center><p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.</p></center>
---
+++ main.js
@@ -0,0 +1,39 @@
+// THIS CODE WAS TAKEN FROM THE Electron DEMO.
+
+const electron = require('electron')
+const {Menu, MenuItem} = electron;
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 1280,
+      height: 720,
+      resizable: false,
+      frame: true,
+      fullscreen: false,
+      title: 'test project'
+  })
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.on('closed', function () {
+    mainWindow = null
+
+  })
+}
+
+app.on('ready', function() {
+    createWindow();
+});
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
